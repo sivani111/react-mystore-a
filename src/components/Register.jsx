c2125cb15a78d9c5997cca522c9985159dd46990
sivani111 <manisivani1105@gmail.com>
2025-03-25 12:27:45 +0530

    a

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,20 +1,33 @@
 import './Register.css'
-import React from 'react'
+import React, { use } from 'react'
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
 export default function Register() {
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
+  const [msg, setMsg] = useState("");
   const handleSubmit = () => {
-    setUsers([...users, user]);
+    const found= users.find((value)=>value.email === user.email);
+    if (found){
+      setMsg("User exists!!!!")
+    }
+    else{
+      setMsg("")
+      setUsers([...users, user]);
+      setUser({ name:"", email:"", password:""});
+    }
+  };
+  const handleDelete = (email) => {
+    setUsers(users.filter((value) => value.email != email));
   };
   return (
     <div className='App-Register-Row'>
     <div className='App-Register-Box'>
       <h3>Registration Form</h3>
-      <p><input type="text" placeholder='Enter Name' onChange={(e) => setUser({ ...user, name: e.target.value })}></input></p>
-      <p><input type="text" placeholder='Email address' onChange={(e) => setUser({ ...user, email: e.target.value })}></input></p>
-      <p><input type="password" placeholder='New password' onChange={(e) => setUser({ ...user, password: e.target.value })}></input></p>
+      {msg}
+      <p><input type="text" placeholder='Enter Name' value={user.name} onChange={(e) => setUser({ ...user, name: e.target.value })}></input></p>
+      <p><input type="text" placeholder='Email address' value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })}></input></p>
+      <p><input type="password" placeholder='New password' value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })}></input></p>
       <p><button onClick={handleSubmit}>Submit</button></p>
       <p><Link to="../login">Already a member? Login here!</Link></p>
     </div>
